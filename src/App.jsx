d349dfdc18930aea15c2090a2379f9ba77da8964
宜旻 李 <yiming79514@gmail.com>
2026-08-04 22:27:59 +0800

    v0.6.0 Day System

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FlightPage from "./pages/FlightPage";
 
 import Home from "./pages/Home";
 import TripDashboard from "./pages/TripDashboard";
 import ItineraryPage from "./pages/ItineraryPage";
+import FlightPage from "./pages/FlightPage";
 
 export default function App() {
   return (
     <BrowserRouter>
-
       <Routes>
 
         <Route
-          path="/trip/:id/flight"
-          element={<FlightPage />}
+          path="/"
+          element={<Home />}
         />
 
         <Route
@@ -22,12 +21,16 @@ export default function App() {
         />
 
         <Route
+          path="/trip/:id/flight"
+          element={<FlightPage />}
+        />
+
+        <Route
           path="/trip/:id/itinerary"
           element={<ItineraryPage />}
         />
 
       </Routes>
-
     </BrowserRouter>
   );
 }
